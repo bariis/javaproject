9d5c6156822cab1c9cc270ad8f907ee36dca4f65
brktrksvr <brktrksvr@172.20.10.4>
2019-11-27 10:15:40 +0200

    Created the project and started the Vehicle class

---
+++ VehicleSellingPortal/src/Vehicle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brktrksvr
+ */
+public class Vehicle {
+    protected int listingId = 0;
+    protected String name; 
+    protected double price;
+    protected String listingDate;
+    protected int modelYear;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String name, double price, String listingDate, int modelYear) {
+        this.name = name;
+        this.price = price;
+        this.listingDate = listingDate;
+        this.modelYear = modelYear;
+        listingId++;
+    }
+    
+    
+}
